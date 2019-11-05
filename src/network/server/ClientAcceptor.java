8d2a4609c8937b69b8768b4ae93a5b4f8d104314
Joelius300 <bstlng321@gmail.com>
2019-11-05 09:36:07 +0100

    Start with the accept client logic

--- src/network/server/ClientAcceptor.java
+++ src/network/server/ClientAcceptor.java
@@ -1,53 +1,78 @@
 package network.server;
 
+import network.Message;
+import protocol.client2server.JoinGame;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ClientAcceptor implements Runnable {
     private final ExecutorService pool;
     private final ServerSocket serverSocket;
-    private final ArrayList<Socket> clients;
+    private final ConcurrentHashMap<String, Socket> clients;
+    private int clientsWaiting = 0;
 
     public ClientAcceptor(ExecutorService pool, ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
         this.pool = pool;
-        clients = new ArrayList<Socket>();
+        clients = new ConcurrentHashMap<String, Socket>();
     }
 
     public void run() {
         try {
-            // accept 4 clients
-            while () {
+            while (clientsWaiting < NetworkServer.MAX_PLAYER_COUNT) {
                 /*
                  Zunächst wird eine Client-Anforderung entgegengenommen(accept-Methode).
-                 Der ExecutorService pool liefert einen Thread, dessen run-Methode
-                 durch die run-Methode der Handler-Instanz realisiert wird.
-                 Dem Handler werden als Parameter übergeben:
-                 der ServerSocket und der Socket des anfordernden Clients.
+                 Der ExecutorService pool liefert einen Thread, auf welchem gehört wird
+                 ob der Client dem Spiel beitritt. Falls ja, wird er zu den Clients hinzugefügt.
                 */
                 Socket cs = serverSocket.accept();  //warten auf Client-Anforderung
 
                 //starte den Handler-Thread zur Realisierung der Client-Anforderung
-                pool.execute(new Handler(serverSocket, cs));
+                pool.execute(new ClientAdder(clients, cs));
+                clientsWaiting++;
             }
         } catch (IOException ex) {
-            System.out.println("--- Interrupt NetworkService-run");
-        } finally {
-            System.out.println("--- Ende NetworkService(pool.shutdown)");
-            pool.shutdown();  //keine Annahme von neuen Anforderungen
+            System.out.println("ClientAcceptor was interrupted");
+        }
+    }
+
+    private class ClientAdder implements Runnable {
+        private final ConcurrentHashMap<String, Socket> clients;
+        private final Socket client;
+
+        public ClientAdder(ConcurrentHashMap<String, Socket> clients, Socket client){
+            this.clients = clients;
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
             try {
-                //warte maximal 4 Sekunden auf Beendigung aller Anforderungen
-                pool.awaitTermination(4L, TimeUnit.SECONDS);
-                if (!serverSocket.isClosed()) {
-                    System.out.println("--- Ende NetworkService:ServerSocket close");
-                    serverSocket.close();
+                // get the input stream from the connected socket
+                InputStream inputStream = client.getInputStream();
+                // create a DataInputStream so we can read data from it.
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                while(true) {
+                    Message message = (Message) objectInputStream.readObject();
+                    if (message instanceof JoinGame) {
+                        clients.put(((JoinGame)message).getPlayerName(), client);
+                        break;
+                    }
                 }
             } catch (IOException e) {
-            } catch (InterruptedException ei) {
+
+            }
+            catch (ClassNotFoundException e) {
+
             }
         }
     }
