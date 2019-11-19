65105fa02b229f65f85a2e06540ab39dffab1b03
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-11-19 09:30:22 +0100

    Test Implementation von mir.
    Hoffe es hilft dir @jli

--- src/network/client/NetworkServerProxy.java
+++ src/network/client/NetworkServerProxy.java
@@ -2,9 +2,25 @@ package network.client;
 
 import network.Message;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class NetworkServerProxy extends ServerProxy {
-    public NetworkServerProxy(ClientApplicationInterface clientApplication) {
+    private final int Server_Port = 4269;
+    private final String Server_Host = "localhost";
+    private Socket serverSocket;
+
+    public NetworkServerProxy(ClientApplicationInterface clientApplication) throws IOException {
         super(clientApplication);
+        try {
+            serverSocket = new Socket(Server_Host, Server_Port);
+            ClientHandler clientHandler = new ClientHandler(Server_Host);
+            clientHandler.schreibeNachricht(serverSocket, "Nachricht");
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
 
         // start listener wöu gibb het ke ahnig wo dasdmä das darf (hint es isch nid im konstruktor)
         // immer we listener öppis empfaht -> nachricht deserialisiärä und a clientApplication witer gä (handleMessage)
@@ -12,6 +28,13 @@ public class NetworkServerProxy extends ServerProxy {
 
     @Override
     public void send(Message message) {
+        try {
+            ObjectOutputStream serverOutputStream = new ObjectOutputStream(serverSocket.getOutputStream());
+            serverOutputStream.writeObject(message);
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
         // send nachricht über netzwärk (per socket) zum server
     }
 }
