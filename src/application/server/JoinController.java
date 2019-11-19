ff05fd9b61b7bf3c83d340a2bb60e6d1917463dd
wingi11 <jannis.wingeyer@gmail.com>
2019-11-19 09:25:36 +0100

    Added todos, implemented ServerApplicationInterface

--- src/application/server/JoinController.java
+++ src/application/server/JoinController.java
@@ -1,7 +1,9 @@
 package application.server;
 
+import network.Message;
 import network.server.Server;
 import protocol.client2server.ClientMessage;
+import protocol.client2server.JoinGame;
 import protocol.server2client.ErrorMessage;
 import protocol.server2client.PlayerJoined;
 import protocol.server2client.StartGame;
@@ -12,18 +14,22 @@ public class JoinController extends Controller {
         super(server, game);
     }
 
+
     @Override
-    public void handleMessage(ClientMessage msg, String connectionId) {
-        if (game.isRunning()) {
-            server.send(new ErrorMessage("Das spiel läuft bereits."), connectionId);
-        } else if (game.playerExists(msg.getPlayerName())) {
-            server.send(new ErrorMessage("Der Name wird bereits verwendet."), connectionId);
-        } else {
-            Player player = game.createPlayer(msg.getPlayerName());
-            server.broadcast(new PlayerJoined(player.getName(), player.getX(), player.getY()));
-            if (game.isFull()) {
-                game.setRunning(true);
-                server.broadcast(new StartGame(game.getMap()));
+    public void handleMessage(Message message, String connectionId) {
+        if(message instanceof JoinGame) {
+            JoinGame joinMessage = (JoinGame)message;
+            if (game.isRunning()) {
+                server.send(new ErrorMessage("Das spiel läuft bereits."), connectionId);
+            } else if (game.playerExists(joinMessage.getPlayerName())) {
+                server.send(new ErrorMessage("Der Name wird bereits verwendet."), connectionId);
+            } else {
+                Player player = game.createPlayer(joinMessage.getPlayerName(), connectionId);
+                server.broadcast(new PlayerJoined(player.getName(), player.getX(), player.getY()));
+                if (game.isFull()) {
+                    game.setRunning(true);
+                    server.broadcast(new StartGame(game.getMap()));
+                }
             }
         }
     }
