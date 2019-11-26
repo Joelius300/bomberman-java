134617fe1e2cc99552540bf7299f278b3143ef91
janicscherer <janic@scherer.ml>
2019-11-26 09:25:04 +0100

    Test

--- src/main/java/ch/srj/highscore/HighscoreApplication.java
+++ src/main/java/ch/srj/highscore/HighscoreApplication.java
@@ -1,22 +1,12 @@
 package ch.srj.highscore;
 
-import ch.srj.highscore.service.ScoreService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class HighscoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HighscoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner demo(ScoreService service) {
-        return (args) -> {
-            service.addScore("anix", 123.0);
-            service.getScore("anix").forEach(System.out::println);
-        };
     }
 }
