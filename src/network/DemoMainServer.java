d01fd6b197f0773ebf25d41cbda53d30e57ebec9
Joelius300 <bstlng321@gmail.com>
2019-12-03 09:28:24 +0100

    Use try with for server

--- src/network/DemoMainServer.java
+++ src/network/DemoMainServer.java
@@ -12,23 +12,22 @@ import java.util.Scanner;
 public class DemoMainServer {
     public static void main(String[] args) throws IOException {
         ServerApplicationInterface fakeServer = (message, connId) -> System.out.println(((JoinGame) message).getPlayerName() + " mit id " + connId + " : sent message");
-        NetworkServer server = new NetworkServer(fakeServer, 4, 42069);
-        server.startListening();
+        try (NetworkServer server = new NetworkServer(fakeServer, 4, 42069)) {
+            server.startListening();
 
-        System.out.println("Ready? Press enter to broadcast some messages");
-        new Scanner(System.in).nextLine();
+            System.out.println("Ready? Press enter to broadcast some messages");
+            new Scanner(System.in).nextLine();
 
-        // broadcast some demo-messages
-        for (int i = 0; i < 4; i++){
-            try {
-                server.broadcast(new PlayerJoined("Player"+i, 0,0));
+            // broadcast some demo-messages
+            for (int i = 0; i < 4; i++) {
+                try {
+                    server.broadcast(new PlayerJoined("Player" + i, 0, 0));
 
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-        server.close();
     }
 }
