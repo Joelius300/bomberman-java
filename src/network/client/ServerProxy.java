170de67ae21db39b7c24b34271099f77c0b04bf6
Joelius300 <bstlng321@gmail.com>
2019-12-03 09:21:08 +0100

    Add close and connect/startListening method to server and serverProxy

--- src/network/client/ServerProxy.java
+++ src/network/client/ServerProxy.java
@@ -2,23 +2,25 @@ package network.client;
 
 import network.Message;
 
+import java.io.IOException;
+
 /**
  * Diese Klasse repräsentiert den Server auf der Client-Seite und zwar auf Ebene Netzwerkschicht.
  * Sie definiert die Schnittstelle, welche die Netzwerkschicht der Bomberman-Client-Komponente anbietet.
  * Sie ist abstrakt und muss innerhalb der Netzwerkschicht durch Ableitung implementiert werden.
  * Die Bomberman-Client-Komponente muss beim Start ein Objekt von dieser Implementationsklasse
  * erzeugen.
- * 
+ *
  * @author Andres Scheidegger
  *
  */
-public abstract class ServerProxy {
+public abstract class ServerProxy implements AutoCloseable {
   /**
    * Referenz auf dasjenige Objekt innerhalb der Bomberman-Client-Komponente, welches vom Server
    * empfangene Nachrichten verarbeitet (Empfängerobjekt).
    */
   protected final ClientApplicationInterface clientApplication;
-  
+
   /**
    * Konstruktor. Initialisiert das neue ServerProxy-Objekt mit der Referenz auf das Empfängerobjekt.
    * @param clientApplication Das Empfängerobjekt des Bomberman-Clients für Nachrichten.
@@ -26,11 +28,13 @@ public abstract class ServerProxy {
   public ServerProxy(ClientApplicationInterface clientApplication){
     this.clientApplication = clientApplication;
   }
-  
+
   /**
    * Sendet ein Nachrichtenobjekt an den Server. Diese Methode muss innerhalb der Netzwerkschicht
    * implementiert werden.
    * @param message Das Nachrichtenobjekt, welches an den Server gesendet werden soll.
    */
   public abstract void send(Message message);
+
+  public abstract void connect() throws IOException;
 }
--- src/network/server/Server.java
+++ src/network/server/Server.java
@@ -7,17 +7,17 @@ import network.Message;
  * anbietet. Die Klasse ist abstrakt und muss innerhalb der Netzwerkschicht durch Ableitung implementiert
  * werden. Die Bomberman-Server-Komponente muss beim Start ein Objekt von dieser Implementationsklasse
  * erzeugen.
- * 
+ *
  * @author Andres Scheidegger
  *
  */
-public abstract class Server {
+public abstract class Server implements AutoCloseable {
   /**
    * Referenz auf dasjenige Objekt innerhalb der Bomberman-Server-Komponente, welches von den Clients
    * empfangene Nachrichten verarbeitet (Empfängerobjekt).
    */
   protected final ServerApplicationInterface serverApplication;
-  
+
   /**
    * Konstruktor. Initialisiert das neue Server-Objekt mit der Referenz auf das Empfängerobjekt.
    * @param serverApplication Das Empfängerobjekt des Bomberman-Servers für Nachrichten.
@@ -25,7 +25,7 @@ public abstract class Server {
   public Server(ServerApplicationInterface serverApplication) {
     this.serverApplication = serverApplication;
   }
-  
+
   /**
    * Sendet eine Nachricht an einen bestimmten Client. Diese Methode muss innerhalb der Netzwerkschicht
    * implementiert werden.
@@ -33,11 +33,13 @@ public abstract class Server {
    * @param connectionId Die ID der Netzwerkverbindung zum Client.
    */
   public abstract void send(Message message, String connectionId);
-  
+
   /**
    * Sendet eine Nachricht an alle Clients. Diese Methode muss innerhalb der Netzwerkschicht
    * implementiert werden.
    * @param message Das Nachrichtenobjekt, welches versendet werden soll.
    */
   public abstract void broadcast(Message message);
+
+  public abstract void startListening();
 }
